7c9470e3e9faa9479d7077c1ddc8d1b7edfc1c79
Ben Beekman <bbeekman@gmail.com>
2020-02-05 12:48:22 -0800

    added quote quantity range

--- src/containers/QuoteFetcherFn.js
+++ src/containers/QuoteFetcherFn.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import Quote from '../components/quotes/Quote';
 import Button from '../components/commons/button/Button';
-import Select from '../components/commons/Option/Select';
+import Select from '../components/commons/option/Select';
+import Range from '../components/commons/range/Range.js';
 import useCharacters from '../hooks/quotes';
 
 export default function QuoteFetcher() {
 
-  const { character, fetchCharacter, selectedCharacter, setSelectedCharacter } = useCharacters('leela', 10);
+  const { character, fetchCharacter, selectedCharacter, setSelectedCharacter } = useCharacters('bender', 10);
 
   return (
     <>
       <Select onChange={ ({ target }) => setSelectedCharacter(target.value) } />
-      <Button text="Get a new one" handleClick={ fetchCharacter } />
+      <Range min={ 1 } max={ 10 } step={ 1 } />
+      <Button text="Gimme quotes!" handleClick={ fetchCharacter } />
 
       <Quote text={ character.text } image={ character.image } character={ character.character } />
     </>
